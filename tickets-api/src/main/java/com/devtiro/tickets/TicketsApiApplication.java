ba8c19797fc67a6ee8ec7b9040a435219caeea7b
NibrasSameeh <nibras.s@aeturnum.com>
2025-07-08 21:06:58 +0530

    Initial commit

---
+++ tickets-api/src/main/java/com/devtiro/tickets/TicketsApiApplication.java
@@ -0,0 +1,13 @@
+package com.devtiro.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsApiApplication.class, args);
+	}
+
+}
